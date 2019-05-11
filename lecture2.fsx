bf0802534ed9e383d3ed91bdccab2a9a2d1fd80a
Thomas <thomaslienchristensen@gmail.com>
2019-05-12 01:05:19 +0200

    Not finished.

--- lecture2.fsx
+++ lecture2.fsx
@@ -1,6 +1,6 @@
 open System
 
-// #2.1
+// 2.1
 let f = function
     | x when x%5 = 0 -> false
     | x when x%2 = 0 || x%3 = 0 -> true
@@ -11,6 +11,7 @@ f(29);;
 f(30);;
 f(12);;
 
+// 2.2
 let rec pow = function
     | (_, 0) -> ""
     | (s, n) -> s + pow(s, n-1)
@@ -24,6 +25,7 @@ let rec evenL = function
 
 evenL([2; 5; 7; 8; 5; 10; 18; 5; 3; 17]);;
 
+// 4.3
 let rec evenN = function
    | 0 -> []
    | x when x%2 = 0 && x > 0 -> x::evenN(x-1)
@@ -31,6 +33,7 @@ let rec evenN = function
 
 evenN(10);;
 
+// 4.8
 let rec split = function
     | [] -> ([], [])
     | x::y::xs -> 
@@ -39,3 +42,41 @@ let rec split = function
     | _ -> ([], [])
 
 split([1..8])
+
+// 4.9
+let rec zip = function
+   | [], [] -> []
+   | _, [] -> raise <| ArgumentException("Listerne har ikke samme længde")
+   | [], _ -> raise <| ArgumentException("Listerne har ikke samme længde")
+   | x::xs, y::ys -> (x, y)::zip(xs, ys)
+
+zip([1; 2; 3;], [1; 2; 3]);;
+zip([1; 5; 7; 89; 4; 3; 2; 4;], [1; 5; 6; 3; 5; 6; 23; 6]);;
+zip([1; 2; 3;], [1; 2; 3; 4;]);;
+zip([1; 2; 3; 4;], [1; 2; 3;]);;
+
+// 4.12
+// Sum of integers satisfying predicate P
+let rec sumP = function
+   | _, [] -> 0
+   | p, x::xs when p(x) -> x + sumP(p, xs)
+   | p, _::xs -> sumP(p, xs)
+
+let p(x) = x > 5;;
+sumP(p, [2; 6; 8; 4; 3; 10; 6; 15;]);; // = 45
+sumP(p, [5; 6]);; // = 6
+sumP(p, [10; 3; 29; 7]);; // = 46
+
+// Paper & Pencil
+// 4.16 Find the types
+// int * int list -> int list
+let rec q = function
+| (x, []) -> []
+| (x, y::ys) -> (x+y)::q(x-1, ys);;
+
+q(5, [1; 2; 3; 3; 3; 3; 3; 3; 3; 3; 3; 3; 10; 7;]);;
+
+
+let rec g = function
+| [] -> []
+| (x,y)::s -> (x,y)::(y,x)::g s;;
